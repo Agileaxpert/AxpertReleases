91ecf880b829193bd5509ceacc9ab21c171f994f
SyamalaQA <syamala@agile-labs.com>
2025-05-05 20:07:20 +0530

    commiting large .js files

--- Version 11.3/Release19/AxpertARM/ARMAPI/wwwroot/js/jquery.timeentry.package/jquery.timeentry-sk.js
+++ Version 11.3/Release19/AxpertARM/ARMAPI/wwwroot/js/jquery.timeentry.package/jquery.timeentry-sk.js
@@ -1,1 +1,3 @@
-(function(a){a.timeEntry.regional.sk={show24Hours:false,separator:":",ampmPrefix:"",ampmNames:["AM","PM"],spinnerTexts:["Teraz","Predchádzajúce pole","Nasledujúce pole","Zvýšiť","Znížiť"]};a.timeEntry.setDefaults(a.timeEntry.regional.sk)})(jQuery);
+version https://git-lfs.github.com/spec/v1
+oid sha256:4eff10bd705026d0f93bc69d4da3e32f584007de13bb58361d74fcacaabc9151
+size 158519583
